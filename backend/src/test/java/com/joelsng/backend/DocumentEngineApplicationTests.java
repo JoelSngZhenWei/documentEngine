c99057acba9c936b0bb4db2d2a9206592598ca46
JoelSngZhenWei <joelsngzw@gmail.com>
2025-08-08 22:09:28 +0800

    Initial commit

---
+++ backend/src/test/java/com/joelsng/backend/DocumentEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joelsng.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
